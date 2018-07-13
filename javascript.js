dc7fe385bc70d2b6e04ab70a65a4796ed8b0aec7
Alex <infanti.alexandre@gmail.com>
2018-07-13 12:17:11 +0200

    pagination img fixed

--- javascript.js
+++ javascript.js
@@ -5,23 +5,25 @@ var pagin1 = document.getElementById('pag01');
 var pagin2 = document.getElementById('pag02');
 var pagin3 = document.getElementById('pag03');
 
+photos2.style.display = "none";
+photos3.style.display = "none";
 
 pagin1.onclick = function() {
-  photos1.style.display = "block";
+  photos1.style.display = "";
   photos2.style.display = "none";
   photos3.style.display = "none";
 }
 
 pagin2.onclick = function() {
   photos1.style.display = "none";
-  photos2.style.display = "block";
+  photos2.style.display = "";
   photos3.style.display = "none";
 }
 
 pagin3.onclick = function() {
   photos1.style.display = "none";
   photos2.style.display = "none";
-  photos3.style.display = "block";
+  photos3.style.display = "";
 }
 
 function sendthis() {
